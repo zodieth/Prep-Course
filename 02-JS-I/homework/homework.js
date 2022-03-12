e1bcdce6495ca436b927e68e6dfcf5369e0a5946
zodieth <contact.prodzodi@gmail.com>
2022-03-12 16:38:44 -0300

    hm1 test

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -7,7 +7,7 @@ const nuevaString = 'Hola';
 const nuevoNum = 2;
 
 // Crea una variable booleana:
-const nuevoBool = true;
+const nuevoBool = false;
 
 // Resuelve el siguiente problema matemático:
 const nuevaResta = 10 - null === 5;
